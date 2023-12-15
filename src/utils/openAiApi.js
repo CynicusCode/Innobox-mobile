58f02780531375f4096dd8dd2ad2a7241b802406
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2023-12-15 10:28:18 -0600

    Successfully connected with OpenAI newest model

--- src/utils/openAiApi.js
+++ src/utils/openAiApi.js
@@ -2,15 +2,29 @@
 import axios from "axios";
 import { OPENAI_API_KEY } from "@env";
 
-// Update the model name here
-const OPENAI_API_URL =
-  "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions";
-
-export const generateResponse = async (prompt) => {
+export const generateResponse = async (email, context, personality, tone) => {
+  console.log(email, context, personality, tone);
   try {
     const response = await axios.post(
-      OPENAI_API_URL,
-      { prompt: prompt, max_tokens: 350 },
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are an email assistant. Your task is to draft a response to a client's email, using internal knowledge about a situation without explicitly stating that knowledge to the user. The response should be proactive and should reflect the given personality and tone.",
+          },
+          {
+            role: "user",
+            content: `${email}`,
+          },
+          {
+            role: "user",
+            content: `As an assistant, you know that ${context}. Now, draft a response with the personality: ${personality}, and tone: ${tone}.`,
+          },
+        ],
+      },
       {
         headers: {
           "Content-Type": "application/json",
@@ -20,8 +34,7 @@ export const generateResponse = async (prompt) => {
     );
 
     console.log("OpenAI API response:", response);
-
-    return response.data.choices[0].text;
+    return response.data.choices[0].message.content;
   } catch (error) {
     console.error(
       "Error calling OpenAI API:",
